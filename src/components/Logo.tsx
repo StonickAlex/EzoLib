fdd7fb0172d69c2bd4d770adf7f8764fa473d675
ssstonick <stonick.alex@gmail.com>
2025-10-20 00:29:50 +0200

    add magic basics

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,7 @@
 export function Logo(props: React.ComponentPropsWithoutRef<'div'>) {
   return (
     <div className="flex items-center space-x-2" {...props}>
-      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-emerald-500 text-white font-bold text-sm">
-        M
-      </div>
+    
       <span className="text-xl font-bold text-zinc-900 dark:text-white">
         Magic Lab
       </span>
